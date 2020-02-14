6b22a969c9ed99af835d57e0a528d9ce231e3037
Ryan Leska <ryan@leskafamily.com>
2020-02-13 19:49:04 -0600

    init of new java timed robot

---
+++ VictorSPBump/src/main/java/frc/robot/Map.java
@@ -0,0 +1,8 @@
+package frc.robot;
+
+public class Map{
+    public static final int fr = 0;
+    public static final int fl = 1;
+    public static final int br = 2;
+    public static final int bl = 3;
+}
